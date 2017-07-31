42d20fc698c707ddefdfe021789e02a795a00c24
Keylin Herrera Esquivel <kherrera@b7-11.local>
2017-07-31 09:58:24 -0600

    Add Hit Functions

--- js/dealer.js
+++ js/dealer.js
@@ -8,7 +8,29 @@ function Dealer() {
 
 	self.deal = function() {};
 
-	self.hit = function() {};
+	self.hit = function() {
+		dealerCards = [];
+		playerCards = [];
+
+		var deckLength = deck.cards.length;
+
+		for (var i = 0; i < deckLength; i++) {
+			
+			//Next Card, push New Card to Dealer Hand
+			if (dealerCards.length < 1 || dealerCards < 5) {
+				var nextCard = deck.cards[i];
+				deck.cards.pop(i);
+				dealerCards.push(nextCard);
+			};
+
+			//Next Card, push New Card to Player Hand
+			if (playerCards < 1 || playerCards < 5) {
+				var nextCard = deck.cards[i];
+				deck.cards.pop(i);
+				playerCards.push(nextCard);
+			};
+		}
+	};
 
 	self.stand = function() {};
 
@@ -22,20 +44,63 @@ function Dealer() {
 
 function Deck() {
 
-	var deck = [];
+	this.cards = [];
+	this.numCards = 0;
 
-	function Ace(value) {}
-	function FaceCard(value) {}
-	function RegularCard(value) {}
+	//Card values
+	var suits = ["spade", "diamond", "heart", "club"];
+	var names = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+	var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
 
-	function Club() {}
-	function Spade() {}
-	function Diamond() {}
-	function Heart() {}
-
-	return deck;
-
+	//Create All Cards
+	for (var s = 0; s < suits.length; s++) {
+    	for (var n = 0; n < names.length; n++) {
+        	this.cards.push(new Card(values[n], names[n], suits[s]));
+     	}
+ 	};
 }
+
+Deck.prototype = {
+	contructor : Deck,
+
+	//Create Ramdom Deck 
+	deckRandom : function() {
+		Deck();
+		this.randomDeck = new Array();
+		var empty = false;
+
+		while(!empty){
+			var randomIndex = Math.floor(Math.random()*this.cards.length);
+			this.randomDeck.push(this.cards[randomIndex]);
+			this.cards.splice(randomIndex, 1);
+			if(this.cards.length <=0) empty = true;
+		}
+
+		for(var i=0; i<this.randomDeck.length; i++){
+			this.cards[i] = this.randomDeck[i];
+		}
+	},
+	
+};
+
+function Card (value, name, suit) {
+	this.value = value;
+	this.suit = suit;
+	this.name = name;
+}
+
+Card.prototype.setValue = function () {
+	if (this.name == "J" || "Q" || "K") {
+		return [10];
+	} else if ( this.name == "A"){
+		return [1, 11];
+	} else {
+		return parseInt(this.name, 10);
+	}
+};
+
+var deck = new Deck();
+deck.deckRandom();
 
 function Play() {
 
